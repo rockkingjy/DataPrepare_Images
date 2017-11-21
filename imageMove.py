d1e9fd47d440907612c75bd7695ac08ea2d29a68
rockkingjy <rockking.jy@gmail.com>
2017-11-21 12:30:28 +0900

    check results

--- imageMove.py
+++ imageMove.py
@@ -7,16 +7,22 @@ import glob
 import os.path
 
 
-originalfolder = "/home/rk/Amy/aerial/aerialImg/"
-destinationfolder = "/home/rk/Amy/aerial/temp/"
+originalfolder = "/home/rk/Amy/aerial/aerialImgnondeleted4/"
+destinationfolder = "/home/rk/Amy/aerial/aerialImgnondeleted4_back/"
+ori = "/home/rk/Amy/aerial/aerialImgnondeleted4.txt"
 
-length = len(glob.glob(originalfolder + "*"))
-if length != 0:
-    print str(length) + " picture left."
-    for i in range(2000):
-        filename = os.path.basename(sorted(glob.glob(originalfolder+"*"))[0])
-        os.rename(originalfolder + filename, destinationfolder + filename)
-    print "Move finished."
+with open(ori,'rU') as f:
+    lines = f.readlines()
+    length = len(lines)
+    print length
+if not os.path.exists(destinationfolder):
+    os.makedirs(destinationfolder)
+
+for i in range(length):
+    filename = lines[i]
+    print i, filename
+    os.rename(originalfolder + filename.rstrip(), destinationfolder + filename.rstrip())
+print "Move finished."
 
 """
 originalfolder = "/home/rk/Amy/aerial/temp/"
